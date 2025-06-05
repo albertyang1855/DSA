ceb758c5721640df20bc576fc98e2923881e37c3
albert <albertyang185@gmail.com>
2025-06-04 23:41:04 -0700

    - Observer

---
+++ src/Observer/Program.cs
@@ -0,0 +1,14 @@
+﻿using Observer.domain.Impl;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
+
+        weatherData.SetMeasurements(22.5f, 65f, 1013.1f);
+        weatherData.SetMeasurements(23.0f, 70f, 1012.5f);
+    }
+}
