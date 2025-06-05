038c34a7303dd4f7e0e89221b9942f2fd1430582
albert <albertyang185@gmail.com>
2025-06-04 22:46:12 -0700

    - Strategy pattern

---
+++ src/Program.cs
@@ -0,0 +1,26 @@
+﻿using Strategy.domain.strategy.Base;
+using Strategy.domain.strategy.Impl.ConcreteBehaviors;
+using Strategy.domain.strategy;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Duck mallard = new MallardDuck();
+        mallard.Display();
+        mallard.PerformFly();
+        mallard.PerformQuack();
+
+        Console.WriteLine();
+
+        Duck rubber = new RubberDuck();
+        rubber.Display();
+        rubber.PerformFly();
+        rubber.PerformQuack();
+
+        // Dynamically change behavior
+        Console.WriteLine("\n-- Rubber duck learns to fly with rocket! --");
+        rubber.SetFlyBehavior(new FlyWithWings());
+        rubber.PerformFly();
+    }
+}
